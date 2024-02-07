cd75b5f393dcbed06abe5c53786efdc432252b72
Mohd Shadab <89182232+shadabibrahim@users.noreply.github.com>
2024-02-08 00:53:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
+// function insert_Row() {
+//     //Write your code here
+//   let table = document.getElementById("sampleTable");
+	
+//   let newRow = document.createElement('tr');
+//   let td1 =document.createElement('td');
+//   let td2 =document.createElement('td');
+// 	td1.innerText = "New Cell1";
+// 	td2.innerText = "New Cell2";
+// 	newRow.appendChild(td1);
+// 	newRow.appendChild(td2);
+
+// 	table.prepend(newRow);
+	
+// }
+
 function insert_Row() {
     //Write your code here
-  let table = document.getElementById("sampleTable");
-	
-  let newRow = document.createElement('tr');
-  let td1 =document.createElement('td');
-  let td2 =document.createElement('td');
-	td1.innerText = "New Cell1";
-	td2.innerText = "New Cell2";
-	newRow.appendChild(td1);
-	newRow.appendChild(td2);
+	var table = document.getElementById("sampleTable");
+    var newRow = table.insertRow(0); // Inserts a new row at index 1 (just after the header row)
 
-	table.prepend(newRow);
+    var cell1 = newRow.insertCell(0); // Inserts a cell at index 0
+    var cell2 = newRow.insertCell(1);
+	// Inserts a cell at index 1
+
+    // You can set the content of the cells here, for example:
+    cell1.innerHTML = "New Cell1";
+    cell2.innerHTML = "New Cell2";
+	 
 	
+  
+  
 }
