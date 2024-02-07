bad9db1ac0b10b445745238e1ff7886574b37adf
Mohd Shadab <89182232+shadabibrahim@users.noreply.github.com>
2024-02-08 00:31:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function insert_Row() {
     //Write your code here
-  
-  
+  let newRow = document.createElement('tr');
+  let td1 =document.createElement('td');
+  let td2 =document.createElement('td');
+	td1.innerText = "New Cell1";
+	newRow.appendChild(td1);
+	td2.innerText = "New Cell2";
+	newRow.appendChild(td2);
+
+  let table = document.getElementById("sampleTable");
+	table.prepend(newRow);
+	
 }
